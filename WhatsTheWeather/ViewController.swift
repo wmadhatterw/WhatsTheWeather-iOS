a9f7dc43ca58d97dbe769feb2bd51362aa2cda31
Seth Walton <sdwalton83@gmail.com>
2018-08-21 15:16:08 -0500

    Initial Commit

---
+++ WhatsTheWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatsTheWeather
+//
+//  Created by Seth Walton on 8/21/18.
+//  Copyright © 2018 Seth Walton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
